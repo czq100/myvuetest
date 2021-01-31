e2f0e00ac382ea105f9c10561eac82ee1432d593
czq <czq100@163.com>
2021-01-31 23:26:16 +0800

    22222

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div>
-    <div style="width:120px;height:50px;border:1px solid;">
-      <Item
-        :isActive="isActive==`dianjing`"
-        @clickaa="clickaa"
-      >电竞</Item>
-    </div>
+  <div class="channel-list">
+    <Channel @clicka1="clicka1"></Channel>
   </div>
 </template>
 
 <script>
-import Item from "./components/Item";
+import Channel from "./components/Channel.vue";
 export default {
-  components: { Item },
-  data() {
-    return { isActive:"" };
-  },
+  components: { Channel },
   methods: {
-    clickaa(e) {
-      this.isActive = !this.isActive;
+    clicka1(e) {
       console.log(e);
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.channel-list {
+  width: 100%;
+  overflow: hidden;
+}
 </style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,10 +1,5 @@
 <template>
-  <div
-    class="item"
-    :class="{active:isActive}"
-    @click="clicka"
-  >
-    <div>{{isActive}}</div>
+  <div class="item" :class="{active:isActive}" @click="clicka">
     <slot></slot>
     <!--插槽就是子组件中的提供给父组件使用的一个占位符，用<slot></slot> 表示，父组件可以在这个占位符中填充任何模板代码，如 HTML、组件等，填充的内容会替换子组件的<slot></slot>标签。 -->
   </div>
@@ -12,12 +7,15 @@
 
 <script>
 export default {
+
   props: {
     isActive: { type: Boolean, require: false, default: true }, //约束属性值
   },
-  methods: { clicka() {
-    this.$emit("clickaa",100);
-  } },
+  methods: {
+    clicka() {
+      this.$emit("clicka2","abcd");
+    },
+  },
 };
 </script>
 
@@ -27,7 +25,7 @@ export default {
   cursor: pointer;
   width: 100%;
   height: 100%;
-  transition: 0.1s;   /*延迟变化属性变化 */
+  transition: 0.1s; /*延迟变化属性变化 */
 }
 .item:hover {
   background-color: azure;
