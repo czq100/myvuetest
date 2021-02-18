cc39af52cc8054b309af054b65d7b2f58fd2c52d
程志强 <zhiqiang.cheng@zhaopin.com.cn>
2021-02-18 14:13:32 +0800

    2222

---
+++ src/components/Icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <i class="iconfont" :class="[iconClass,extraClass]"></i>
+</template>
+
+<script>
+var icons = [
+  "icon-07lianjie",
+  "icon-08jiechulianjie",
+  "icon-19beijingyanse",
+  "icon-19beijingyanse",
+  "icon-20xiangmufuhao",
+  "icon-05xiabiao",
+  "icon-5",
+];
+export default {
+  props: {
+    num: {
+      type: Number,
+      require: true,
+    },
+    extraClass: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    iconClass() {
+      return icons[this.num];
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "//at.alicdn.com/t/font_2362095_ul1lxrdbd6b.css";
+</style>
